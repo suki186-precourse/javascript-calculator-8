a8cab4c3b2a09f228c6a5b9172cff922f6740e89
Suki <tues1205@naver.com>
2025-10-17 15:54:34 +0900

    feat(App): 사용자에게 문자열 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      // 덧셈할 문자열 입력
+      const input = await Console.readLineAsync(
+        "덧셈할 문자열을 입력해 주세요.\n"
+      );
+      Console.print(`입력 값: ${input}`);
+    } catch (error) {
+      Console.print(error.message);
+      throw error; // 나중에 Calculator에서 발생한 에러 감지
+    }
+  }
 }
 
 export default App;
