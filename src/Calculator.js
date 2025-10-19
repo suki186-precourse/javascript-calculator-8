addfa078955c139d1e7953ae9c132f1688f8b1ba
Suki <tues1205@naver.com>
2025-10-19 18:14:15 +0900

    refactor(Calculator): sum 함수 책임 분리

--- src/Calculator.js
+++ src/Calculator.js
@@ -10,27 +10,41 @@ export default class Calculator {
     this.input = input;
   }
 
-  // 더하기 함수
+  // 덧셈 계산기 함수
   sum() {
     // 1. 빈 문자열 체크 -> 합계 0 처리
     if (validateEmptyInput(this.input)) return 0;
 
-    // 2. 구분자 추출
-    const { delimiter, numbersString } = getDelimiterAndNumber(this.input);
+    // 2. 구분자 추출, 유효성 검사
+    const { delimiter, numbersString } = this.#parseDelimiter();
 
-    // 3. 구분자 유효성 검사
-    validateDelimiter(delimiter, numbersString);
+    // 3. 구분자를 기준으로 문자열 분리, 양의 정수 검증
+    const numbers = this.#parseNumbers(delimiter, numbersString);
 
-    // 4. 구분자를 기준으로 문자열 분리
-    const splitNumbers = numbersString.split(delimiter);
-
-    // 5. 양의 정수 검증
-    validatePositiveInteger(splitNumbers);
-
-    // 6. 문자를 숫자로 변환 후 합 계산
-    const numbers = splitNumbers.map(Number);
-    const sum = numbers.reduce((acc, cur) => acc + cur, 0);
+    // 4. 합 계산
+    const sum = this.#calculateSum(numbers);
 
     return sum;
+  }
+
+  // 구분자 추출, 유효성 검사 함수
+  #parseDelimiter() {
+    const { delimiter, numbersString } = getDelimiterAndNumber(this.input);
+    validateDelimiter(delimiter, numbersString);
+
+    return { delimiter, numbersString };
+  }
+
+  // 문자열 분리, 양의 정수 검증 함수
+  #parseNumbers(delimiter, numbersString) {
+    const splitNumbers = numbersString.split(delimiter);
+    validatePositiveInteger(splitNumbers);
+
+    return splitNumbers.map(Number);
+  }
+
+  // 합 계산 함수
+  #calculateSum(numbers) {
+    return numbers.reduce((acc, cur) => acc + cur, 0);
   }
 }
