347a0ae3621b11135beb42571f1c799a3c2fef08
Suki <tues1205@naver.com>
2026-01-04 21:56:49 +0900

    chore(constants): 예외처리 상수 정의

--- src/constants.js
+++ src/constants.js
@@ -11,3 +11,16 @@ export const DELIMITERS = Object.freeze({
   CUSTOM_START: "//",
   CUSTOM_END: "\\n",
 });
+
+// ===== 예외 처리
+export const ERROR_PREFIX = "[ERROR]";
+
+export const ERROR_MESSAGES = Object.freeze({
+  // 1. 양수
+  INVALID_NUMBER: `${ERROR_PREFIX} 양수만 입력 가능합니다.`,
+  EMPTY_NUMBER: `${ERROR_PREFIX} 더할 숫자들을 입력해 주세요.`,
+
+  // 2. 구분자
+  INVALID_DELIMITER: `${ERROR_PREFIX} 숫자는 구분자로 사용할 수 없습니다.`,
+  DUPLICATE_DELIMITER: `${ERROR_PREFIX} 구분자를 연속으로 사용할 수 없습니다.`,
+});
