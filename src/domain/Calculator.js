9c49edcb22e6ffc0ee4163fb0bc1c65a755ce709
Suki <tues1205@naver.com>
2026-01-04 21:48:03 +0900

    feat(Calculator): 커스텀 구분자 포함한 계산기 구현

--- src/domain/Calculator.js
+++ src/domain/Calculator.js
@@ -5,10 +5,8 @@ class Calculator {
     // 빈 문자열 처리
     if (!input) return 0;
 
-    // 문자열 분리
-    // 숫자 배열 변환
-    const regex = new RegExp(`[${DELIMITERS.DEFAULT.join("")}]`);
-    const numbers = input.split(regex).map(Number);
+    // 문자열 분리, 숫자 배열 변환
+    const numbers = this.#parseInput(input);
 
     // 합계 계산
     const sum = numbers.reduce((acc, cnt) => acc + cnt, 0);
@@ -16,9 +14,29 @@ class Calculator {
     return sum;
   }
 
-  // #parseInput (input) {
-  //     // 구분자 추출
-  //     // 문자열 분리
-  // }
+  #parseInput(input) {
+    // 커스텀 구분자 추출
+    const match = input.match(/^\/\/(.)\\n/); // 커스텀 구분자 있는지
+    let customDelimiter;
+    let targetString;
+
+    // 분리할 문자열 확정
+    if (match) {
+      // 커스텀 구분자 있는 경우
+      customDelimiter = match[1];
+      targetString = input.split(`//${customDelimiter}\\n`)[1];
+    } else {
+      // 없는 경우
+      customDelimiter = new RegExp(`[${DELIMITERS.DEFAULT.join("")}]`);
+      targetString = input;
+    }
+
+    // 문자열 분리, 숫자 배열 변환 (기본, 커스텀 모두)
+    const numbers = targetString.split(customDelimiter).map(Number);
+    console.log(numbers);
+
+    // 결과 배열 반환
+    return numbers;
+  }
 }
 export default Calculator;
