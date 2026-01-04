55bccf1135a813682836dffb0dd01ebc9469ee5d
Suki <tues1205@naver.com>
2026-01-04 20:36:27 +0900

    chore(constants): 상수 정의

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+export const INPUT_MESSAGES = Object.freeze({
+  START: "덧셈할 문자열을 입력해 주세요.",
+});
+
+export const OUTPUT_MESSAGES = Object.freeze({
+  RESULT: "결과 : ",
+});
+
+export const DELIMITERS = Object.freeze({
+  DEFAULT: [",", ":"],
+  CUSTOM_START: "//",
+  CUSTOM_END: "\\n",
+});
